d9b36fd560ae6f6e51310a3f41e507cc56689586
no name <your-email@example.com>
2025-07-19 14:03:20 +0530

    added default screen weather details

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
+import DefaultScreen from "./components/DefaultScreen";
 
 const App = () => {
   return (
     <div className="app">
       <Header />
+      <DefaultScreen/>
       <p className="copyright-text">&copy; 2025 KIRA. All Rights Reserved</p>
     </div>
   );
